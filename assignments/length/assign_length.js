718ec1ee375a4d762b12a2db4cfb55c8fa7f6427
Moses Negus <35280933+mosesnegus@users.noreply.github.com>
2021-11-18 21:31:05 -0800

    a

---
+++ assignments/length/assign_length.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+
+class SLL {
+    // constructor, other methods, removed for brevity
+    length() {
+    	// how many nodes are in my list?
+    }
+ }
