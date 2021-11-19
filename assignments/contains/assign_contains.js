f6a75807be7be2d43013fd6057e0e94bc0a54bce
Moses Negus <35280933+mosesnegus@users.noreply.github.com>
2021-11-18 21:27:27 -0800

    a

---
+++ assignments/contains/assign_contains.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+
+
+class SLL {
+    // constructor, other methods, removed for brevity
+    contains(value) {
+    	// is "value" to be found anywhere in this list?
+    }
+ }
