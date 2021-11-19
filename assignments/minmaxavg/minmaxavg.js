d31e606fdd1de10c3a3bdea27fcbade89cdc28c4
Moses Negus <35280933+mosesnegus@users.noreply.github.com>
2021-11-18 21:38:20 -0800

    a

---
+++ assignments/minmaxavg/minmaxavg.js
@@ -0,0 +1,21 @@
+class SLL {
+    // constructor, other methods, removed for brevity
+    max() {
+    	// identify the largest value in the list
+    }
+ }
+
+ class SLL {
+    // constructor, other methods, removed for brevity
+    min() {
+    	// identify the smallest value in the list
+    }
+ }
+
+ class SLL {
+    // constructor, other methods, removed for brevity
+    average() {
+    	// what is the average value in my list?
+    }
+ }
+
